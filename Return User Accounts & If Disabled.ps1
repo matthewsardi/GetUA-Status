6f04a215b0aaa3bc000cac0dbfbd2cd312ac3fc9
matthewsardi <119958810+matthewsardi@users.noreply.github.com>
2023-01-16 12:15:14 -0500

    Add files via upload

---
+++ Return User Accounts & If Disabled.ps1
@@ -0,0 +1,17 @@
+﻿Import-Module activedirectory
+
+$ResultList = Get-Content -Path \\navitus.local\navitus\Home\atw1115f\Documents\termedaccounts.txt | ForEach-Object {
+    Get-ADUser -LDAPFilter "(anr=$_)" | Select-Object -Property samaccountname,name,enabled
+}
+
+
+#write-host $ResultList
+$ResultList | Export-Csv -Path \\navitus.local\navitus\Home\atw1115f\Documents\Results.csv -NoTypeInformation 
+
+#foreach($user in $gpADusers){
+ #   $splitUsers = $user.Split(',')
+  #  $ADUsers = $splitUsers.trimStart().trimEnd()
+   # foreach($id in $ADUsers){
+    #    get-aduser -Identity $id | select name,samaccountname
+    #}
+#}
